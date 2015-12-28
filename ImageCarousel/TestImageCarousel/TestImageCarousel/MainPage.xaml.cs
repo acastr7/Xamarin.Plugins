a7d9528fb40110c5c9a866e347a5fc28c9f0d97f
Moshe Taieb <moosat@gmail.com>
2015-12-28 17:37:33 +0200

    ImageCarousel Support URLS - Lazy loading and recycle controls

--- ImageCarousel/TestImageCarousel/TestImageCarousel/MainPage.xaml.cs
+++ ImageCarousel/TestImageCarousel/TestImageCarousel/MainPage.xaml.cs
@@ -10,8 +10,6 @@ namespace TestImageCarousel
 		public MainPage ()
 		{
 			InitializeComponent ();
-			//btn.Clicked += Btn_Clicked;
-			MyCarousel.Images = new System.Collections.ObjectModel.ObservableCollection<Image> ();
 			ImageUrls = new List<string> () {
 				"http://cdn.zenfolio.net/img/s10/v114/p93242074-3.jpg?sn=2YH",
 				"http://cdn.zenfolio.net/img/s6/v137/p256857143-3.jpg?sn=2YH",
@@ -22,23 +20,10 @@ namespace TestImageCarousel
 				"http://cdn.zenfolio.net/img/s7/v155/p330729244-3.jpg?sn=2YH",
 				"http://cdn.zenfolio.net/img/s6/v139/p61290126-3.jpg?sn=2YH"
 			};
-			Images = new List<Image> ();
-
-			foreach (var url in ImageUrls) {
-				var image = new Image ();
-				image.Source = ImageSource.FromUri (new Uri (url));
-				Images.Add (image);
-			}
 		}
 
 		public void LoadImagesClicked (object sender, EventArgs e)
 		{
-//			foreach (var image in Images) {
-//				MyCarousel.Images.Add (image);
-//
-//			}
-			//MyCarousel.ImageUrls.Clear();
-//			//MyCarousel.Images.Clear ();
 			foreach (var url in ImageUrls) {
 				MyCarousel.ImageUrls.Add (url);
 
@@ -46,7 +31,6 @@ namespace TestImageCarousel
 		}
 
 		private static List<string> ImageUrls;
-		private static List<Image> Images;
 	}
 }
 
